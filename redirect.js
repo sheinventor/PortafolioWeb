2f03bfeab07efbf3c3335407bcc24bf6ea29fa72
PaoVazBar <110428835+sheinventor@users.noreply.github.com>
2025-05-21 15:37:50 +0200

    Update Spanish

--- redirect.js
+++ redirect.js
@@ -1,9 +1,14 @@
 const lang = navigator.language || navigator.userLanguage;
 
-if (lang.startsWith('fr')) {
-  window.location.href = 'fr.html';
-} else if (lang.startsWith('en')) {
-  window.location.href = 'en.html';
-} else {
-  window.location.href = 'index.html';
+// Solo redirige si no se ha redirigido antes
+if (!localStorage.getItem('languageRedirected')) {
+  localStorage.setItem('languageRedirected', 'true');
+
+  if (lang.startsWith('fr')) {
+    window.location.href = 'fr.html';
+  } else if (lang.startsWith('en')) {
+    window.location.href = 'en.html';
+  } else {
+    window.location.href = 'index.html';
+  }
 }
